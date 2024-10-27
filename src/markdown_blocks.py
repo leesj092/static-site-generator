cde650d4230285a27703410dd55baf8117c9b79d
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-10-28 04:17:29 +0900

    added block parser tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -47,3 +47,8 @@ def block_to_block_type(block):
         return block_type_olist
     return block_type_paragraph
 
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
