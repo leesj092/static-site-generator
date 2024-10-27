3eca741d72c23fea18e08b4903e73ae1b7c9503d
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-10-28 01:56:51 +0900

    added link and image detector

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,13 +1,6 @@
 from textnode import TextType, TextNode
+import re
 
-#node = TextNode("This is text with a `code block` word", TextType.TEXT)
-#new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-
-#[
-#    TextNode("This is text with a ", TextType.TEXT),
-#    TextNode("code block", TextType.CODE),
-#    TextNode(" word", TextType.TEXT),
-#]
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
 
@@ -28,3 +21,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(new_node)
 
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r'!\[([^\[\]]*)\]\(([^\(\)]*)\)', text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r'(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)', text)
+    return matches
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,8 +1,12 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_links,
+    extract_markdown_images,
 )
+
 from textnode import TextNode, TextType
+
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
@@ -63,7 +67,7 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode("**bold** and *italic*", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
-        self.assertListEqual(
+        self.assertEqual(
             [
                 TextNode("bold", TextType.BOLD),
                 TextNode(" and ", TextType.TEXT),
@@ -82,6 +86,30 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
         )
 
 
