111eebc6c11135246d224957b36eabc539ccd4ef
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-10-28 04:05:38 +0900

    added block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,19 +1,49 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks(markdown):
-    splits = markdown.split('\n')
+    splits = markdown.split('\n\n')
     blocks = []
-    line = ''
 
-    for i in range(len(splits) - 1):
-        if splits[i] == '':
+    for split in splits:
+        if split == '':
             continue
-
-        if splits[i]:
-            if line:
-                line += '\n' + splits[i].strip()
-            else:
-                line = splits[i].strip()
-        if not splits[i+1]:
-            blocks.append(line)
-            line = ''
+        blocks.append(split.strip())
 
     return blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
+
