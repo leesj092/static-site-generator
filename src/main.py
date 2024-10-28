3b387b5877224058819c29ba2410ca77e1609d51
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-10-29 03:38:31 +0900

    static to public generation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 from textnode import *
-from markdown_parser import split_nodes_delimiter
+import os
+import shutil
 
 def main():
     node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    copy_source_to_destination('static', 'public')
+
+def copy_source_to_destination(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    os.mkdir(destination)
+
+    entries = os.listdir(source)
+
+    for entry in entries:
+        if os.path.isfile(f'{source}/{entry}'):
+            shutil.copy(f'{source}/{entry}', destination)
+        else:
+            copy_source_to_destination(f'{source}/{entry}', f'{destination}/{entry}')
 
 if __name__ == '__main__':
     main()
